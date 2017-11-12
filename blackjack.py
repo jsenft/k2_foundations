9112b51aa0524fa1fa4e96aa9e5365c461e6bba3
John Senft <jsenft2001@yahoo.com>
2017-11-12 15:09:22 -0800

    Blackjack game debugging

--- blackjack.py
+++ blackjack.py
@@ -4,6 +4,7 @@ cards = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 1
 
 card_list = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
+game = True
 def pick_card():
     card = random.choice(card_list)
     return card
@@ -34,8 +35,50 @@ add_values(dealer_card2, dealer)
 add_values(player_card, player)
 add_values(player_card2, player)
 print('Player showing: ', sum(player))
+while(game):
+    if sum(dealer) != 21 or sum(player) != 21:
+        hit = input('Player hits? (y/n) ')
+        if hit == 'y':
+            player_card = pick_card()
+            add_values(player_card, player)
+            print('Player draws: ', player_card)
+            print('Player showing: ', sum(player))
+            if sum(player) > 21:
+                print('Player busts! Dealer wins!')
+                break
+            elif sum(player) == 21:
+                print('Blackjack! Player wins')
+                break
+        elif hit == 'n':
+            if sum(player) > sum(dealer):
+                print("Dealer's 2nd card: %s" % dealer_card2)
+                print('Dealer showing: ', sum(dealer))
+                while(sum(dealer) < sum(player) and sum(dealer) < 21):
+                    dealer_card = pick_card()
+                    print('Dealer draws: %s' % dealer_card)
+                    add_values(dealer_card, dealer)
+                    print('Dealer showing: ', sum(dealer))
+                    if sum(dealer) > 21:
+                        print('Dealer busts! Player wins!')
+                        game = False
+                        break
+                    elif sum(dealer) == 21:
+                        print('Blackjack! Dealer wins')
+                        game = False
+                        break
+                    elif sum(dealer) >= sum(player):
+                        print('Dealer wins!')
+                        game = False
+                        break
+        else:
+            print('Invalid input. Try again')
 
-
-#hit = input('Player hits? y/n')
-#if hit == 'y' or 'Y':
-#    player_card = pick_card()
+    else:
+        if sum(dealer) == 21:
+            print("Dealer's 2nd card: %s" % dealer_card2)
+            print('Blackjack! Dealer wins!')
+            game = False
+        else:
+            print("Player's 2nd card: %s" % dealer_card2)
+            print('Blackjack! Player wins!')
+            game = False
