407964b762c40e8a6b0e6eb74f2a268ca67fa6b6
John Senft <jsenft2001@yahoo.com>
2017-11-14 13:08:45 -0800

    Removed game redundancies

--- blackjack.py
+++ blackjack.py
@@ -1,6 +1,6 @@
 import random
 
-cards = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10, 'A': (1, 11)}
+cards = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10, 'A': 11}
 
 card_list = list(cards.keys())
 
@@ -9,6 +9,7 @@ def pick_card():
     card = random.choice(card_list)
     return card
 
+# Initial game setup
 dealer = []
 player = []
 dealer_card = pick_card()
@@ -17,30 +18,19 @@ dealer_card2 = pick_card()
 player_card2 = pick_card()
 print('Dealer draws: %s' % dealer_card)
 print('Player draws: ' + player_card + ' and ' + player_card2)
-
-def add_values(card, values):
-    if sum(values)  <= 10:
-        if card == 'A':
-            values.append(cards['A'][1])
-        else:
-            values.append(cards[card])
-    else:
-        if card == 'A':
-            values.append(cards['A'][0])
-        else:
-            values.append(cards[card])
-
-add_values(dealer_card, dealer)
-add_values(dealer_card2, dealer)
-add_values(player_card, player)
-add_values(player_card2, player)
+dealer.append(cards[dealer_card])
+dealer.append(cards[dealer_card2])
+player.append(cards[player_card])
+player.append(cards[player_card2])
 print('Player showing: ', sum(player))
+
+# Game logic that inteacts with player
 while(game):
     if sum(dealer) != 21 and sum(player) != 21:
         hit = input('Player hits? (y/n) ')
         if hit == 'y':
             player_card = pick_card()
-            add_values(player_card, player)
+            player.append(cards[player_card])
             print('Player draws: ', player_card)
             print('Player showing: ', sum(player))
             if sum(player) > 21:
@@ -62,7 +52,7 @@ while(game):
                 while(sum(dealer) < sum(player) and sum(dealer) < 21):
                     dealer_card = pick_card()
                     print('Dealer draws: %s' % dealer_card)
-                    add_values(dealer_card, dealer)
+                    dealer.append(cards[dealer_card])
                     print('Dealer showing: ', sum(dealer))
                     if sum(dealer) > 21:
                         if 11 in dealer:
@@ -82,7 +72,7 @@ while(game):
         else:
             print('Invalid input. Try again')
 
-    else:
+    else: # This runs when dealer/player get initial blackjack
         if sum(dealer) == 21:
             print("Dealer's 2nd card: %s" % dealer_card2)
             print('Blackjack! Dealer wins')
