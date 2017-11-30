18b0a7186a46f222c4d033a2e845f23eb3fb07b0
John Senft <jsenft2001@yahoo.com>
2017-11-30 11:16:44 -0800

    Refactored game logic

--- blackjack.py
+++ blackjack.py
@@ -33,80 +33,41 @@ player_score = total_cards(player)
 dealer_score = total_cards(dealer)
 print('Player showing: ', player_score)
 
-while(game):
-    if player_score < 21:
-        hit = input('Player hits? (y/N) ')
-        if hit != 'y':
-            if player_score >= dealer_score:
-                print("Dealer's 2nd card: %s" % dealer_card2)
-                print('Dealer showing: ', dealer_score)
-                while(dealer_score <= player_score or dealer_score > 17 and dealer_score < 21):
-                    dealer_card = pick_card()
-                    print('Dealer draws: %s' % dealer_card)
-                    dealer.append(cards[dealer_card])
-                    dealer_score = total_cards(dealer)
-                    print('Dealer showing: ', dealer_score)
-                    if dealer_score > 21:
-                        print('Dealer busts. Player wins!')
-                        game = False
-                        break
-                    elif dealer_score > player_score:
-                        print('Dealer wins')
-                        game = False
-                        break
-                    elif dealer_score < player_score:
-                        print('Player wins')
-                        game = False
-                        break
-                    elif dealer_score == player_score:
-                        print('Game is a push')
-                        game = False
-                        break
-            else:
-                print("Dealer's 2nd card: %s" % dealer_card2)
-                print('Dealer showing: ', dealer_score)
-                if dealer_score < player_score:
-                    print('Player wins!')
-                    game = False
-                    break
-                elif dealer_score > player_score:
-                    print('Dealer wins')
-                    game = False
-                    break
-                else:
-                    print('Game is a push')
-                    game = False
-                    break
+#Player interaction logic
+while(player_score < 21):
+    hit = input('Player hits? (y/N) ')
+    if hit == 'y':
+        player_card = pick_card()
+        player.append(cards[player_card])
+        player_score = total_cards(player)
+        print('Player draws: ', player_card)
+        print('Player showing: ', player_score)
+    else:
+        break
 
-        else:
-            player_card = pick_card()
-            player.append(cards[player_card])
-            player_score = total_cards(player)
-            print('Player draws: ', player_card)
-            print('Player showing: ', player_score)
+if player_score > 21:
+    print('Player busts. Dealer wins')
 
-    elif player_score == 21:
-        print("Dealer's 2nd card: %s" % dealer_card2)
-        print('Dealer showing: ', dealer_score)
-        while(dealer_score <= player_score or dealer_score > 17 and dealer_score < 21):
+#Dealer response and game outcome resolution
+else:
+    print("Dealer's 2nd card: %s" % dealer_card2)
+    print('Dealer showing: ', dealer_score)
+    while(dealer_score < 17):
+        if dealer_score <= player_score:
             dealer_card = pick_card()
             print('Dealer draws: %s' % dealer_card)
             dealer.append(cards[dealer_card])
             dealer_score = total_cards(dealer)
             print('Dealer showing: ', dealer_score)
-            if dealer_score > 21:
-                print('Dealer busts. Player wins!')
-                game = False
-                break
-            elif dealer_score > player_score:
-                print('Dealer wins')
-                game = False
-                break
-            elif dealer_score < player_score:
-                print('Player wins')
-                game = False
-                break
-            elif dealer_score == player_score:
-                print('Game is a push')
-                game = False
-                break
+        else:
+            break
+
+    if dealer_score > player_score:
+        if dealer_score <= 21:
+            print('Dealer wins')
+        else:
+            print('Dealer busts. Player wins!')
+    elif dealer_score == player_score:
+        print('Game is a push')
+    else:
+        print('Player wins!')
